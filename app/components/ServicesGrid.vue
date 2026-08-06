ff93bce8d234b0238f5cc50ffdf735b3de95a293
mark andre stephens <127994059+markandrevegas@users.noreply.github.com>
2026-08-06 20:31:34 +0200

    fixed img tags

--- app/components/ServicesGrid.vue
+++ app/components/ServicesGrid.vue
@@ -6,7 +6,7 @@ import workshopImg from '~/assets/images/workshop.webp'
 <template>
 	<div class="text-palladian grid h-full w-full gap-4 px-8 sm:grid-cols-2 md:grid-cols-4">
 		<div class="relative flex-shrink-0">
-			<<img :src="meetingImg" class="h-auto w-full" alt="Events">
+			<img :src="meetingImg" class="h-auto w-full" alt="Events">
 			<div class="absolute inset-0 z-10 flex flex-col justify-end gap-1 p-8">
 				<p class="font-display text-xl/6 font-semibold">Møder og strategidage</p>
 				<p class="font-light">Hold møde med panoramaudsigt ud over vandet og til Københavns spir og tage.</p>
@@ -15,7 +15,7 @@ import workshopImg from '~/assets/images/workshop.webp'
 		</div>
 
 		<div class="relative flex-shrink-0">
-			<<img :src="eventsImg" class="h-auto w-full" alt="Events">
+			<img :src="eventsImg" class="h-auto w-full" alt="Events">
 			<div class="absolute inset-0 z-10 flex flex-col justify-end gap-1 p-8">
 				<p class="font-display text-xl/6 font-semibold">Events og receptioner</p>
 				<p class="font-light">Skab en helt særlig ramme om jeres arrangement med byens liv og vandets ro som baggrundstæppe.</p>
@@ -23,7 +23,7 @@ import workshopImg from '~/assets/images/workshop.webp'
 			<div class="absolute inset-0 bg-gradient-to-t from-black/80 to-transparent" />
 		</div>
 		<div class="relative flex-shrink-0">
-			<<img :src="workshopImg" class="h-auto w-full" alt="Events">
+			<img :src="workshopImg" class="h-auto w-full" alt="Events">
 			<div class="absolute inset-0 z-10 flex flex-col justify-end gap-1 p-8">
 				<p class="font-display text-xl/6 font-semibold">Workshops og teambuilding</p>
 				<p class="font-light">Omsæt indsigt til handling. Floating G.O.A.T. er et perfekt sted til at tænke nye tanker og blive inspireret.</p>
@@ -31,7 +31,7 @@ import workshopImg from '~/assets/images/workshop.webp'
 			<div class="absolute inset-0 bg-gradient-to-t from-black/80 to-transparent" />
 		</div>
 		<div class="relative flex-shrink-0">
-			<<img :src="eventsImg" class="h-auto w-full" alt="Events">
+			<img :src="eventsImg" class="h-auto w-full" alt="Events">
 			<div class="absolute inset-0 z-10 flex flex-col justify-end gap-1 p-8">
 				<p class="font-display text-xl/6 font-semibold">Workshops og teambuilding</p>
 				<p class="font-light">Omsæt indsigt til handling. Floating G.O.A.T. er et perfekt sted til at tænke nye tanker og blive inspireret.</p>
