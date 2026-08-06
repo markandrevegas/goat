547067a68c7fb825053ead21a1fce4bfb7c52813
mark andre stephens <127994059+markandrevegas@users.noreply.github.com>
2026-08-06 09:40:44 +0200

    services

--- app/components/ServicesGrid.vue
+++ app/components/ServicesGrid.vue
@@ -1,34 +1,38 @@
 <script setup lang="ts"></script>
 <template>
-	<div class="grid h-full w-full gap-4 px-8 sm:grid-cols-2 md:grid-cols-4 text-palladian">
+	<div class="text-palladian grid h-full w-full gap-4 px-8 sm:grid-cols-2 md:grid-cols-4">
 		<div class="relative flex-shrink-0">
-			<NuxtImg src="/images/meeting.webp" class="w-full h-auto" alt="Events" />
-			<div class="absolute inset-0 z-10 flex flex-col justify-end p-8">
-				<p class="text-xl">Møder og strategidage</p>
-				<p class="text-lg">Hold møde med panoramaudsigt ud over vandet og til Københavns spir og tage.</p>
+			<NuxtImg src="/images/meeting.webp" class="h-auto w-full" alt="Events" />
+			<div class="absolute inset-0 z-10 flex flex-col gap-1 justify-end p-8">
+				<p class="text-xl font-display font-semibold">Møder og strategidage</p>
+				<p class="font-light">Hold møde med panoramaudsigt ud over vandet og til Københavns spir og tage.</p>
 			</div>
+			<div class="absolute inset-0 bg-gradient-to-t from-black/60 to-transparent" />
 		</div>
-		
+
 		<div class="relative flex-shrink-0">
-			<NuxtImg src="/images/events.webp" class="w-full h-auto" alt="Events" />
-			<div class="absolute inset-0 z-10 flex flex-col justify-end p-8">
-				<p class="text-xl">Events og receptioner</p>
-				<p class="text-lg">Skab en helt særlig ramme om jeres arrangement med byens liv og vandets ro som baggrundstæppe.</p>
+			<NuxtImg src="/images/events.webp" class="h-auto w-full" alt="Events" />
+			<div class="absolute inset-0 z-10 flex flex-col gap-1 justify-end p-8">
+				<p class="text-xl font-display font-semibold">Events og receptioner</p>
+				<p class="font-light">Skab en helt særlig ramme om jeres arrangement med byens liv og vandets ro som baggrundstæppe.</p>
 			</div>
-		</div>
-		<div class="relative flex-shrink-0">
-			<NuxtImg src="/images/workshop.webp" class="w-full h-auto" alt="Events" />
-			<div class="absolute inset-0 z-10 flex flex-col justify-end p-8">
-				<p class="text-xl">Workshops og teambuilding</p>
-				<p class="text-lg">Omsæt indsigt til handling. Floating G.O.A.T. er et perfekt sted til at tænke nye tanker og blive inspireret.</p>
-			</div>
+			<div class="absolute inset-0 bg-gradient-to-t from-black/60 to-transparent" />
 		</div>
 		<div class="relative flex-shrink-0">
-			<NuxtImg src="/images/ferry.webp" class="w-full h-auto" alt="Events" />
-			<div class="absolute inset-0 z-10 flex flex-col justify-end p-8">
-				<p class="text-xl">Workshops og teambuilding</p>
-				<p class="text-lg">Omsæt indsigt til handling. Floating G.O.A.T. er et perfekt sted til at tænke nye tanker og blive inspireret.</p>
+			<NuxtImg src="/images/workshop.webp" class="h-auto w-full" alt="Events" />
+			<div class="absolute inset-0 z-10 flex flex-col gap-1 justify-end p-8">
+				<p class="text-xl font-display font-semibold">Workshops og teambuilding</p>
+				<p class="font-light">Omsæt indsigt til handling. Floating G.O.A.T. er et perfekt sted til at tænke nye tanker og blive inspireret.</p>
 			</div>
+			<div class="absolute inset-0 bg-gradient-to-t from-black/60 to-transparent" />
+		</div>
+		<div class="relative flex-shrink-0">
+			<NuxtImg src="/images/ferry.webp" class="h-auto w-full" alt="Events" />
+			<div class="absolute inset-0 z-10 flex flex-col gap-1 justify-end p-8">
+				<p class="text-xl font-display font-semibold">Workshops og teambuilding</p>
+				<p class="font-light">Omsæt indsigt til handling. Floating G.O.A.T. er et perfekt sted til at tænke nye tanker og blive inspireret.</p>
+			</div>
+			<div class="absolute inset-0 bg-gradient-to-t from-black/60 to-transparent" />
 		</div>
 	</div>
 </template>
