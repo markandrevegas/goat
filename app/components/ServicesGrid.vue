66329d3db38dcd3e9ea4e5759251a0aa3a4a0306
mark andre stephens <127994059+markandrevegas@users.noreply.github.com>
2026-08-06 09:32:53 +0200

    services

--- app/components/ServicesGrid.vue
+++ app/components/ServicesGrid.vue
@@ -1,17 +1,34 @@
 <script setup lang="ts"></script>
 <template>
-	<div class="grid w-full flex-1 gap-4 px-8 sm:grid-cols-2 md:grid-cols-4">
-		<div>
-			<NuxtImg src="/images/meeting.webp" class="h-full w-full" alt="Events" />
+	<div class="grid h-full w-full gap-4 px-8 sm:grid-cols-2 md:grid-cols-4 text-palladian">
+		<div class="relative flex-shrink-0">
+			<NuxtImg src="/images/meeting.webp" class="w-full h-auto" alt="Events" />
+			<div class="absolute inset-0 z-10 flex flex-col justify-end p-8">
+				<p class="text-xl">Møder og strategidage</p>
+				<p class="text-lg">Hold møde med panoramaudsigt ud over vandet og til Københavns spir og tage.</p>
+			</div>
 		</div>
-		<div>
-			<NuxtImg src="/images/events.webp" class="h-full w-full" alt="Events" />
+		
+		<div class="relative flex-shrink-0">
+			<NuxtImg src="/images/events.webp" class="w-full h-auto" alt="Events" />
+			<div class="absolute inset-0 z-10 flex flex-col justify-end p-8">
+				<p class="text-xl">Events og receptioner</p>
+				<p class="text-lg">Skab en helt særlig ramme om jeres arrangement med byens liv og vandets ro som baggrundstæppe.</p>
+			</div>
 		</div>
-		<div>
-			<NuxtImg src="/images/workshop.webp" class="h-full w-full" alt="Events" />
+		<div class="relative flex-shrink-0">
+			<NuxtImg src="/images/workshop.webp" class="w-full h-auto" alt="Events" />
+			<div class="absolute inset-0 z-10 flex flex-col justify-end p-8">
+				<p class="text-xl">Workshops og teambuilding</p>
+				<p class="text-lg">Omsæt indsigt til handling. Floating G.O.A.T. er et perfekt sted til at tænke nye tanker og blive inspireret.</p>
+			</div>
 		</div>
-		<div>
-			<NuxtImg src="/images/ferry.webp" class="h-full w-full" alt="Events" />
+		<div class="relative flex-shrink-0">
+			<NuxtImg src="/images/ferry.webp" class="w-full h-auto" alt="Events" />
+			<div class="absolute inset-0 z-10 flex flex-col justify-end p-8">
+				<p class="text-xl">Workshops og teambuilding</p>
+				<p class="text-lg">Omsæt indsigt til handling. Floating G.O.A.T. er et perfekt sted til at tænke nye tanker og blive inspireret.</p>
+			</div>
 		</div>
 	</div>
 </template>
