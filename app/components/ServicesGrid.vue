621a7ae1c944ff55e0b0c237bf04ca3781908076
mark andre stephens <127994059+markandrevegas@users.noreply.github.com>
2026-08-10 22:27:52 +0200

    compress images and set sizes

--- app/components/ServicesGrid.vue
+++ app/components/ServicesGrid.vue
@@ -5,8 +5,8 @@ import workshopImg from "~/assets/images/workshop.webp"
 </script>
 <template>
 	<div class="text-palladian grid h-full w-full gap-4 px-8 sm:grid-cols-2 md:grid-cols-4">
-		<div class="relative flex-shrink-0">
-			<NuxtImg :src="meetingImg" loading="lazy" class="h-auto w-full" alt="Events"></NuxtImg>
+		<div class="relative w-full">
+			<NuxtImg :src="meetingImg"  sizes="100vw sm:50vw md:25vw" loading="lazy" class="h-auto w-full" alt="Events"></NuxtImg>
 			<div class="absolute inset-0 z-10 flex flex-col justify-end gap-1 p-8">
 				<p class="font-display text-xl/6 font-semibold">Møder og strategidage</p>
 				<p class="font-light">Hold møde med panoramaudsigt ud over vandet og til Københavns spir og tage.</p>
@@ -14,24 +14,24 @@ import workshopImg from "~/assets/images/workshop.webp"
 			<div class="absolute inset-0 bg-gradient-to-t from-black/80 to-transparent" />
 		</div>
 
-		<div class="relative flex-shrink-0">
-			<NuxtImg :src="eventsImg" loading="lazy" class="h-auto w-full" alt="Events"></NuxtImg>
+		<div class="relative w-full">
+			<NuxtImg :src="eventsImg" sizes="100vw sm:50vw md:25vw" loading="lazy" class="h-auto w-full" alt="Events"></NuxtImg>
 			<div class="absolute inset-0 z-10 flex flex-col justify-end gap-1 p-8">
 				<p class="font-display text-xl/6 font-semibold">Events og receptioner</p>
 				<p class="font-light">Skab en helt særlig ramme om jeres arrangement med byens liv og vandets ro som baggrundstæppe.</p>
 			</div>
 			<div class="absolute inset-0 bg-gradient-to-t from-black/80 to-transparent" />
 		</div>
-		<div class="relative flex-shrink-0">
-			<NuxtImg :src="workshopImg" loading="lazy" class="h-auto w-full" alt="Events"></NuxtImg>
+		<div class="relative w-full">
+			<NuxtImg :src="workshopImg"  sizes="100vw sm:50vw md:25vw" loading="lazy" class="h-auto w-full" alt="Events"></NuxtImg>
 			<div class="absolute inset-0 z-10 flex flex-col justify-end gap-1 p-8">
 				<p class="font-display text-xl/6 font-semibold">Workshops og teambuilding</p>
 				<p class="font-light">Omsæt indsigt til handling. Floating G.O.A.T. er et perfekt sted til at tænke nye tanker og blive inspireret.</p>
 			</div>
 			<div class="absolute inset-0 bg-gradient-to-t from-black/80 to-transparent" />
 		</div>
-		<div class="relative flex-shrink-0">
-			<NuxtImg :src="eventsImg" loading="lazy" class="h-auto w-full" alt="Events"></NuxtImg>
+		<div class="relative w-full">
+			<NuxtImg :src="eventsImg" sizes="100vw sm:50vw md:25vw" loading="lazy" class="h-auto w-full" alt="Events"></NuxtImg>
 			<div class="absolute inset-0 z-10 flex flex-col justify-end gap-1 p-8">
 				<p class="font-display text-xl/6 font-semibold">Simple Meetings</p>
 				<p class="font-light">Omsæt indsigt til handling. Floating G.O.A.T. er et perfekt sted til at tænke nye tanker og blive inspireret.</p>
