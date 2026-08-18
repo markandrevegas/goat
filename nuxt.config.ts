c1d262a17e61110c4b75fa86a16e6369c3b2e0ea
mark andre stephens <127994059+markandrevegas@users.noreply.github.com>
2026-08-18 12:59:21 +0200

    changed root folder

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -64,7 +64,7 @@ declare module "nuxt/schema" {
 }
 export default defineNuxtConfig({
 	app: {
-		baseURL: process.env.NODE_ENV === "production" ? "/app/" : "/",
+		baseURL: "/",
 		head: {
 			htmlAttrs: {
 				lang: "da-DK"
