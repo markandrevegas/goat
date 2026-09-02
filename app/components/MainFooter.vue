530e9a5841af5e76c4d56a8700940c8029544b19
mark andre stephens <127994059+markandrevegas@users.noreply.github.com>
2026-09-02 21:47:43 +0200

    footer

--- app/components/MainFooter.vue
+++ app/components/MainFooter.vue
@@ -22,30 +22,30 @@ const pageItems = computed(() => {
 })
 </script>
 <template>
-	<footer class="text-base/6">
-		<div class="grid flex-shrink-0 grid-cols-1 gap-4 p-8 py-24 sm:mx-auto sm:max-w-6xl sm:grid-cols-2 md:grid-cols-4">
+	<footer class="text-base/7">
+		<div class="grid flex-shrink-0 grid-cols-1 gap-8 p-8 py-24 sm:mx-auto sm:max-w-6xl sm:grid-cols-2 md:grid-cols-4">
 			<div class="flex flex-col gap-1">
-				<p class="font-display text-lg font-semibold">Floating G.O.A.T.</p>
-				<p>Langebrogade 1C<br />1411 København K<br />CVR: 44307987</p>
+				<p class="font-display text-xl font-semibold mb-2">Floating G.O.A.T.</p>
+				<p class="text-base/7">Langebrogade 1C<br />1411 København K<br />CVR: 44307987</p>
 			</div>
 			<div class="flex flex-col">
-				<p class="font-display text-lg font-semibold">Kontakt</p>
-				<p>
+				<p class="font-display text-xl font-semibold mb-2">Kontakt</p>
+				<p class="text-base/7">
 					info@floatinggoat.dk<br />
 					Telefontid: Hverdage 9-18<br />
 					+45 30104262
 				</p>
 			</div>
 			<div class="flex flex-col">
-				<p class="font-display text-lg font-semibold">Information</p>
-				<ul class="list-reset space-y-2">
+				<p class="font-display text-xl font-semibold mb-2">Information</p>
+				<ul class="list-reset">
 					<li v-for="item in pageItems" :key="item.id">
 						<NuxtLink :to="item.slug" class="underline transition-opacity duration-400 hover:decoration-2">{{ item.title.rendered }}</NuxtLink>
 					</li>
 				</ul>
 			</div>
 			<div class="flex flex-col">
-				<p class="font-display text-lg font-semibold">Find os</p>
+				<p class="font-display text-xl font-semibold mb-2">Find os</p>
 				<div class="fill-palladian flex w-full justify-start gap-4">
 					<Instagram class="size-6" />
 					<Facebook class="size-6" />
