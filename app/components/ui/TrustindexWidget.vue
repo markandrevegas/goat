7964eb36f4e58f1b10383acfdc626b001fbbe111
mark andre stephens <127994059+markandrevegas@users.noreply.github.com>
2026-09-15 21:38:46 +0200

    reviews layout

--- app/components/ui/TrustindexWidget.vue
+++ app/components/ui/TrustindexWidget.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
-import Airbnb from '../icons/Airbnb.vue'
-import StarFilled from '../icons/StarFilled.vue'
-import BadgeChecked from '../icons/BadgeChecked.vue'
-import QuoteMark from '../icons/QuoteMark.vue'
+import Airbnb from "../icons/Airbnb.vue"
+import StarFilled from "../icons/StarFilled.vue"
+import BadgeChecked from "../icons/BadgeChecked.vue"
+import QuoteMark from "../icons/QuoteMark.vue"
 
 interface Reviewer {
 	firstName: string
@@ -29,66 +29,66 @@ const { data, pending, error } = await useFetch<AirbnbApiResponse>("https://floa
 </script>
 
 <template>
-	<section class="bg-palladian text-brand w-full overflow-hidden px-4 py-12 font-sans md:px-8">
+	<section class="bg-palladian text-brand flex h-[80vh] w-full flex-col justify-center overflow-hidden px-4 font-sans md:h-screen md:px-8">
 		<div v-if="pending" class="text-brand text-center font-bold">Loading reviews...</div>
 
 		<div v-else-if="error" class="text-center font-bold text-red-800">Could not load reviews.</div>
 
-		<div v-else-if="data?.success" class="mx-auto flex max-w-6xl flex-col items-center gap-2 lg:flex-row lg:items-stretch">
-
-			<div class="flex min-w-[250px] shrink-0 flex-col items-center justify-center">
-        <!-- Airbnb Logo text approximation (Red) -->
-				<div>
-					<Airbnb class="scale-175" />
-				</div>
-				<h2 class="tracking-tight mt-6 mb-2 text-xl font-display md:text-3xl">Excellent</h2>
+		<div v-else-if="data?.success" class="mx-auto flex w-full flex-col items-center gap-8 lg:flex-row lg:items-stretch">
+			<div class="mx-auto flex w-[250px] shrink-0 flex-col items-center justify-center">
+				<h2 class="font-display mb-2 text-xl tracking-tight md:text-3xl">Excellent</h2>
 
 				<!-- 5 Large Stars -->
 				<div class="mb-2 flex">
-          <StarFilled v-for="i in 5" :key="i" />
+					<StarFilled v-for="i in 5" :key="i" class="size-6" />
 				</div>
 
-				<p class="mb-4 text-sm font-medium">
-					Based on <NuxtLink :to="'https://www.airbnb.com/rooms/1097007987468938423'" target="_blank"><span class="font-bold underline decoration-2 underline-offset-2">{{ data.reviews_count }} reviews</span></NuxtLink>
+				<p class="mt-2 mb-6 text-sm font-medium">
+					Based on
+					<NuxtLink :to="'https://www.airbnb.com/rooms/1097007987468938423'" target="_blank" class="font-bold underline decoration-2 underline-offset-2 hover:opacity-70">{{ data.reviews_count }} reviews</NuxtLink>
 				</p>
 
-				
+				<!-- Airbnb Logo text approximation (Red) -->
+				<div>
+					<Airbnb class="scale-175" />
+				</div>
 			</div>
 
-			<div class="flex justify-start items-center w-full snap-x snap-mandatory [scrollbar-width:none] gap-4 overflow-x-auto scroll-smooth px-2 pt-2 pb-6 [-ms-overflow-style:none] lg:snap-none lg:overflow-scroll lg:gap-4 [&::-webkit-scrollbar]:hidden">
-
-				<div v-for="review in data.reviews" :key="review.id" class="flex  snap-start min-w-[320px] h-72 flex-col rounded-2xl bg-white p-6 shadow-sm sm:w-[calc(50%-0.5rem)] lg:w-[320px]">
+			<div class="flex w-full snap-x snap-mandatory [scrollbar-width:none] items-center justify-start gap-4 overflow-x-auto scroll-smooth px-2 pt-2 pb-6 [-ms-overflow-style:none] lg:snap-none lg:gap-4 lg:overflow-scroll [&::-webkit-scrollbar]:hidden">
+				<div v-for="review in data.reviews" :key="review.id" class="flex h-72 min-w-[320px] snap-start flex-col rounded-2xl bg-white p-6 shadow-sm sm:w-[calc(50%-0.5rem)] lg:w-[320px]">
 					<!-- Reviewer Header -->
 					<div class="mb-3 flex items-center gap-x-3">
 						<img :src="review.reviewer?.pictureUrl" :alt="review.reviewer?.firstName" class="h-12 w-12 rounded-full object-cover" />
 						<div>
-							<h3 class="text-lg/5 font-bold">
-								{{ review.reviewer?.firstName }}
-							</h3>
-							<p class="text-[12px] font-medium text-brand/60">
+							<div class="flex items-start justify-start">
+								<h3 class="text-base/5 font-bold">
+									{{ review.reviewer?.firstName }}
+								</h3>
+								<BadgeChecked class="ml-1 text-blue-500" />
+							</div>
+
+							<p class="text-brand/60 text-xs/5 font-medium">
 								{{ review.localizedDate }}
 							</p>
 						</div>
 					</div>
 
 					<!-- Stars & Verified Badge -->
-					<div class="mb-4 flex items-center gap-1">
-            <StarFilled v-for="i in review.rating" :key="i" />
-            <BadgeChecked class="text-blue-500 ml-1" />
+					<div class="-gap-[.1rem] mb-4 flex items-center">
+						<StarFilled v-for="i in review.rating" :key="i" class="size-4" />
 					</div>
 
 					<!-- Review Text -->
-					<p class="mb-4 line-clamp-4 flex-grow text-sm/5 text-brand">
+					<p class="text-brand mb-4 line-clamp-4 flex-grow text-xs/5">
 						{{ review.localizedText || review.text }}
 					</p>
 
 					<!-- Footer: Read More & Quote Icon -->
 					<div class="mt-auto flex justify-end">
 						<!-- Large Quote Icon -->
-            <QuoteMark class="size-8" />
+						<QuoteMark class="size-8" />
 					</div>
 				</div>
-
 			</div>
 		</div>
 	</section>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -97,11 +97,11 @@ useSeoMeta({
 			:speed="20"
 			class="hidden"
 		/>-->
-		<TrustindexWidget v-if="showReviews = 'true'" />
 		<FirstRow />
 		<ClientOnly>
 			<CardScroller :items="standardCards" />
 		</ClientOnly>
+		<TrustindexWidget v-if="showReviews = 'true'" />
 		<ImageScrollWindow :feature-header-sm="featureHeaderSm" :feature-header-lg="featureHeaderLg" :feature-image="featureImage" :feature-text="featureText" :feature-button="featureButton" :scroll-header="scrollHeader" :feature-url="featureUrl" :scroll-image="scrollImage" />
 		<FerryVideo :header="ferryVideoHeader" :header-sm="ferryVideoHeaderSm" :text="ferryVideoText" :url="ferryVideoUrl" :button="ferryVideoButton" />
 	</NuxtLayout>
