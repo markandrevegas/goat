0268aa688a12dd16156c261b2921da0d09307395
mark andre stephens <127994059+markandrevegas@users.noreply.github.com>
2026-09-24 11:33:08 +0200

    moved preload fonts to layouts/default

--- app.vue
+++ app.vue
@@ -1,32 +1,6 @@
 <script setup lang="ts">
 import { onMounted } from "vue"
 import { useCookieConsent } from "~/app/plugins/cookie-consent.client"
-
-useHead({
-	link: [
-		{
-			rel: "preload",
-			href: "/_fonts/HgJel4iAS5mDVqJaDUYKmxaMJg4zKHTGmyD1FQkOsTM-2hVuuQIZ4GSGMgNv6dvlqz7cfD6lr7xLohUph7OCgFg.woff2",
-			as: "font",
-			type: "font/woff2",
-			crossorigin: "anonymous"
-		},
-		{
-			rel: "preload",
-			href: "/_fonts/pZrE-slef2USYSteHMofh38tG-0x5V8w3-m5O8o55BA-2lkoGCOmO7jeyF1uZ0eej8W_w9soLdR904X6bIAsF0k.woff2",
-			as: "font",
-			type: "font/woff2",
-			crossorigin: "anonymous"
-		},
-		{
-			rel: "preload",
-			href: "/_fonts/pBA_EGUUgytVcAn30G3L7jSPUjTfQBkUF_SVrKXvks4-P8M5mJujzuY5Rvr7oSsGgEy3RgI8I6ZroH9ErPwewNI.woff2",
-			as: "font",
-			type: "font/woff2",
-			crossorigin: "anonymous"
-		}
-	]
-})
 </script>
 
 <template>
