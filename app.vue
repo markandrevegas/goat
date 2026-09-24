4ad376a40f9d43d74d717b7d27f59a7c985dfac1
mark andre stephens <127994059+markandrevegas@users.noreply.github.com>
2026-09-24 11:16:47 +0200

    preload font in app.vue

--- app.vue
+++ app.vue
@@ -1,6 +1,17 @@
 <script setup lang="ts">
 import { onMounted } from "vue"
 import { useCookieConsent } from "~/app/plugins/cookie-consent.client"
+useHead({
+	link: [
+		{
+			rel: "preload",
+			href: "/_fonts/HgJel4iAS5mDVqJaDUYKmxaMJg4zKHTGmyD1FQkOsTM-2hVuuQIZ4GSGMgNv6dvlqz7cfD6lr7xLohUph7OCgFg.woff2",
+			as: "font",
+			type: "font/woff2",
+			crossorigin: "anonymous"
+		}
+	]
+})
 </script>
 
 <template>
